88eb3045b633914faad84aa860d7a696248a6920
Angus Fudge <angf@pm.me>
2021-11-26 20:59:02 -0800

    And some more...

---
+++ ex34.py
@@ -0,0 +1,18 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# 1. The animal at 1.
+print(animals[1] == 'python3.6')
+# 2. The third (3rd) animal.
+print(animals[2] == 'peacock')
+# 3. The first (1st) animal.
+print(animals[0] == 'bear')
+# 4. The animal at 3.
+print(animals[3] == 'kangaroo')
+# 5. The fifth (5th) animal.
+print(animals[4] == 'whale')
+# 6. The animal at 2.
+print(animals[2] == 'peacock')
+# 7. The sixth (6th) animal.
+print(animals[5] == 'platypus')
+# 8. The animal at 4.
+print(animals[4] == 'whale')
