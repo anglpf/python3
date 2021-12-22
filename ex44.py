f9cb807482003602a1f92a9f37c444fe2988a0be
Angus Fudge <angf@pm.me>
2021-12-22 22:41:35 +0000

    Ex44

--- ex44.py
+++ ex44.py
@@ -62,3 +62,46 @@ dad = Father()
 
 gpa.altered()
 dad.altered()
+
+
+# COMPOSITION
+class Other(object):
+
+    def override(self):
+        print("OTHER override()")
+
+    def implicit(self):
+        print("OTHER implicit()")
+
+    def altered(self):
+        print("OTHER altered()")
+
+class Child(object):
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+        print("CHILD, BEFORE OTHER altered()")
+        self.other.altered()
+        print("CHILD, AFTER OTHER altered()")
+
+son = Child()
+son.implicit()
+son.override()
+son.altered()
+
+"""
+Zed's advice:
+
+Use composition to package code into modules that are used in many different unrelated places and situations.
+
+Use inheritance only when there are clearly-related, reusable pieces of code that fit under a single common concept.
+
+"""
